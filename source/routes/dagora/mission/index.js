7dd435f09caa1e989914dc26e660fea458bb96e8
Giang Phan <phanbinhgiang1999@gmail.com>
2023-03-29 15:56:35 +0700

    update

--- source/routes/dagora/mission/index.js
+++ source/routes/dagora/mission/index.js
@@ -2,12 +2,10 @@ import express from 'express'
 import partner from './partner'
 import mission from './mission'
 import taskHistory from './taskHistory'
-import participant from './participantMission'
 
 const router = express.Router()
 router.use('/partner', partner)
 router.use('/mission', mission)
 router.use('/mission/task', taskHistory)
-router.use('/participant', participant)
 
 module.exports = router
--- source/routes/dagora/mission/participantMission.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { ParticipantMissionWorker } from '../../../worker'
-const router = express.Router()
-
-// User
-router.post('/user', ParticipantMissionWorker.createParticipantMission)
-
-export default router
